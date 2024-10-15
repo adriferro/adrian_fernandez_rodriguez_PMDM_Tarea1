e69b1e860f5e492b30a347fc3cd62fca3de18f2f
adriferro <a.fernandezrodriguez2003@gmail.com>
2024-10-15 12:40:23 +0200

    Interfaz login correcto y creacion actividad alarma

--- app/src/main/java/com/example/adrian_fernandez_rodriguez_pmdm_tarea1/LoginCorrecto.java
+++ app/src/main/java/com/example/adrian_fernandez_rodriguez_pmdm_tarea1/LoginCorrecto.java
@@ -1,6 +1,12 @@
 package com.example.adrian_fernandez_rodriguez_pmdm_tarea1;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +25,37 @@ public class LoginCorrecto extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        String username = getIntent().getStringExtra("USERNAME");
+
+        // Referencias a los elementos del layout
+        TextView welcomeText = findViewById(R.id.textViewWelcome);
+        ImageButton imageButtonWeb = findViewById(R.id.imageBtnWeb);
+        Button btnPonerAlarma = findViewById(R.id.btnAlarma);
+
+        // Mostrar el mensaje de bienvenida con el nombre de usuario
+        welcomeText.setText("Bienvenido: " + username);
+
+        // Acción del ImageButton para abrir la página web
+        imageButtonWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent implícito para abrir el navegador con la URL
+                String url = "https://www.tutorialspoint.com/android/android_intents_filters.htm";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
+        // Acción del botón "Poner alarma" que lleva a la actividad 3
+        btnPonerAlarma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent explícito para ir a la actividad de poner la alarma
+                Intent intent = new Intent(LoginCorrecto.this, Alarma.class);
+                startActivity(intent);
+            }
         });
     }
 }
